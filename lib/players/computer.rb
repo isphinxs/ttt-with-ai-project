6c93b17909cad01ecffae13968bda0444c9a8fdc
Samantha Ostrowski <isphinxs@hotmail.com>
2020-12-20 17:46:44 -0500

    commit tree 31546c78329399f45da1d0abae1bc9ab8fb01495

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,7 +1,6 @@
 module Players
     class Computer < Player
         def move(board)
-            # binding.pry
             move = rand(1..9)
             while !board.valid_move?(move)
                 move = rand(1..9)
